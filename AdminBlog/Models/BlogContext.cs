940deb9de7573a7bb2302c9ced9f7034af0bcc5d
Furkan <furkanerbass@hotmail.com>
2020-11-23 17:25:34 +0300

    ilk gönderme

---
+++ AdminBlog/Models/BlogContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminBlog.Models
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=FURKAN-PC\FURKAN;Database=BlogDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public BlogContext(DbContextOptions<BlogContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Blog> Blog { get; set; }
+    }
+}
